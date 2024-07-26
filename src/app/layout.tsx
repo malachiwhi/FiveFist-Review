9f4b1580a973e1a2a69442a3f76d3e96a7d42391
Malachi <malachiwhite31@gmail.com>
2024-07-26 08:30:01 -0400

    Completed NewSub and about to host-2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
         </div>
         <div className="bg - slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"> 
             {children}
-          <Footer/>
         </div> 
       </body>
     </html>
